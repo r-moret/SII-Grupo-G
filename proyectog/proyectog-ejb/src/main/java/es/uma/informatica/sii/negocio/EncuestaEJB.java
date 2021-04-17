18ae364ca0e2a5b17e9127751b041308c9a234f4
Cris-rq <cristinaespejoroque@outlook.es>
2021-04-17 01:41:20 -0700

    terminado metodo registrarEncuesta

--- proyectog/proyectog-ejb/src/main/java/es/uma/informatica/sii/negocio/EncuestaEJB.java
+++ proyectog/proyectog-ejb/src/main/java/es/uma/informatica/sii/negocio/EncuestaEJB.java
@@ -1,32 +1,45 @@
 package es.uma.informatica.sii.negocio;
 
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceUnitUtil;
 
 import es.uma.informatica.sii.exceptions.SecretariaException;
 import es.uma.informatica.sii.proyectog.entidades.Encuesta;
+import es.uma.informatica.sii.proyectog.entidades.Encuesta.EncuestaID;
 import es.uma.informatica.sii.proyectog.entidades.Expediente;
 
-public class EncuestaEJB implements EncuestaInterface{
+public class EncuestaEJB implements EncuestaInterface {
 
-	@PersistenceContext(unitName = "ejb-demo")
-	private EntityManager em;
-	
+	private EntityManagerFactory emf = Persistence.createEntityManagerFactory("proyectog-jpa");
+	private EntityManager em = emf.createEntityManager();
+
 	@Override
 	public void registrarEncuesta(Expediente expediente, Encuesta encuesta) throws SecretariaException {
+
 		Expediente exp = em.find(Expediente.class, expediente.getNumExpediente());
-		if(exp == null) {
-			// El expediente no existe en la base de datos
+
+		if (exp == null) {
+			// El expediente no existe en la bbdd
 			throw new SecretariaException();
 		}
-		
-		/*
-		 * Encuesta enc = em.find(Encuesta.class, encuesta.); if(enc == null) {
-		 * enc.setExpediente(exp); em.persist(enc); } else if (enc != null) {
-		 * 
-		 * }
-		 */
-		
+
+		PersistenceUnitUtil util = emf.getPersistenceUnitUtil();
+		Object idEncuesta = util.getIdentifier(encuesta);
+
+		Encuesta enc = em.find(Encuesta.class, (EncuestaID) idEncuesta);
+
+		if (enc == null) {
+			// La encuesta no se encontraba en la bbdd
+			encuesta.setExpediente(exp);
+			em.persist(encuesta);
+		} else if (enc != null) {
+			// La encuesta estaba en la bbdd y se debe sobreescribir
+			enc.setExpediente(expediente);
+			em.merge(enc);
+		}
+
 	}
 
 	@Override
