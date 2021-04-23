ff07448d2a3a6d575fae0f5ff48fecc77adbf4fd
Cris-rq <cristinaespejoroque@outlook.es>
2021-04-23 08:52:42 -0700

    Corregidos errores en TestExpediente y ExpedienteEJB: [FUNCIONAMIENTO ACTUAL CORRECTO]

--- proyectog/src/test/java/es/uma/informatica/sii/test/negocio/TestExpediente.java
+++ proyectog/src/test/java/es/uma/informatica/sii/test/negocio/TestExpediente.java
@@ -16,10 +16,10 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import es.uma.informatica.sii.entidades.Titulacion;
+import es.uma.informatica.sii.anotaciones.Requisitos;
 import es.uma.informatica.sii.entidades.Alumno;
 import es.uma.informatica.sii.entidades.Matricula;
 import es.uma.informatica.sii.entidades.Expediente;
@@ -75,40 +75,50 @@ public class TestExpediente {
 		emf.close();
 	}
 
+	@Requisitos({ "RF9" })
 	@Test
-	@Ignore
 	public void testActualizarExpediente() {
 		
+		// Caso 1 - paso un expediente nulo
 		try {
 			expedienteEJB.actualizarExpediente(null);
 			fail("Permite actualizar un expediente nulo");
 		}
 		catch(SecretariaException exc1) {
-			try {
-				Expediente exp = new Expediente();
-				exp.setNumExpediente(4);
-				exp.setTitulacion(em.find(Titulacion.class, 1));
-				exp.setAlumno(em.find(Alumno.class, "12W"));
-				List<Matricula> matris = new ArrayList<>();
-				exp.setMatriculas(matris);
-				exp.setActivo(false);
-				
-				expedienteEJB.actualizarExpediente(exp);
-				fail("Permite actualizar un expediente inexistente en la base de datos");
-			}
-			catch(ExpedienteInexistente exc2) {
-				/* COMPORTAMIENTO CORRECTO */
-			}
-			catch(Exception exc2) {
-				fail("Lanza la excepciï¿½n incorrecta 1");
-			}
+			// Excepcion correcta
 		}
 		catch(Exception exc1) {
 			fail("Lanza la excepciï¿½n incorrecta 2");
 		}
 		
-		Expediente exp = em.find(Expediente.class, 1);
+		// Caso 2 - paso un expediente no existente en la bbdd
+		try {
+			Expediente exp = new Expediente();
+			exp.setNumExpediente(4);
+			exp.setTitulacion(em.find(Titulacion.class, 1));
+			
+			Alumno al1 = new Alumno();
+			al1.setDni("12C");
+			al1.setNombreCompleto("Antonio Moret Sánchez");
+			al1.setEmailInstitucional("anton@gmail.es");
+			
+			exp.setAlumno(al1);
+			List<Matricula> matris = new ArrayList<>();
+			exp.setMatriculas(matris);
+			exp.setActivo(false);
+			
+			expedienteEJB.actualizarExpediente(exp);
+			fail("Permite actualizar un expediente inexistente en la base de datos");
+		}
+		catch(ExpedienteInexistente exc2) {
+			/* COMPORTAMIENTO CORRECTO */
+		}
+		catch(Exception exc2) {
+			fail("Lanza la excepcion incorrecta 1");
+		}
 		
+		// Modifico un expediente existente en la bbdd
+		Expediente exp = em.find(Expediente.class, 1);
 		exp.setActivo(true);
 		
 		try {
