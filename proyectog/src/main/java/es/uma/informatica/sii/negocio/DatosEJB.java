69b55429d4dea7252b02c7dacd6d17e929885ec8
CjagEu <cj.alonso@uma.es>
2021-04-24 02:10:22 -0700

    importarDatosAlumnado implementado

--- proyectog/src/main/java/es/uma/informatica/sii/negocio/DatosEJB.java
+++ proyectog/src/main/java/es/uma/informatica/sii/negocio/DatosEJB.java
@@ -7,15 +7,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Timestamp;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
-import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
 
 import es.uma.informatica.sii.entidades.DatosAlumnado;
 
@@ -28,53 +24,51 @@ public class DatosEJB implements DatosEJBInterface {
         InputStream excelStream = null;
         try {
             excelStream = new FileInputStream(excel);
-            // Representación del más alto nivel de la hoja excel.
-            HSSFWorkbook hssfWorkbook = new HSSFWorkbook(excelStream);
-            // Elegimos la hoja que se pasa por parámetro.
+            //Lista de hojas excel.
+            @SuppressWarnings("resource")
+			HSSFWorkbook hssfWorkbook = new HSSFWorkbook(excelStream);
+            //Primera hoja
             HSSFSheet hssfSheet = hssfWorkbook.getSheetAt(0);
-            // Objeto que nos permite leer un fila de la hoja excel, y de aquí extraer el contenido de las celdas.
-            HSSFRow hssfRow;
-            // Inicializo el objeto que leerá el valor de la celda
-            HSSFCell cell;                        
-            // Obtengo el número de filas ocupadas en la hoja
-            int rows = hssfSheet.getLastRowNum();
-            // Obtengo el número de columnas ocupadas en la hoja
-            int cols = 0;            
-            // Cadena que usamos para almacenar la lectura de la celda
-            String cellValue;  
-            // Para este ejemplo vamos a recorrer las filas obteniendo los datos que queremos            
-            for (int r = 0; r < rows; r++) {
-                hssfRow = hssfSheet.getRow(r);
+            //Objeto que nos permite leer un fila de la hoja excel, y de aquí extraer el contenido de las celdas.
+            HSSFRow hssfRow;                    
+            //Obtengo el número de filas ocupadas en la hoja
+            int filas = hssfSheet.getLastRowNum();        
+            //En el documento los datos empiezan en fila 5       
+            for (int f = 5; f < filas; f++) {
+            	 
+                hssfRow = hssfSheet.getRow(f);
                 DatosAlumnado da = new DatosAlumnado();
-                if (hssfRow == null){
+                if (hssfRow == null){ //¿Se puede omitir si sabemos que no habrá filas vacias?
                     break;
-                }else{
-                    for (int c = 0; c < (cols = hssfRow.getFirstCellNum()); c++) {
-                         	da.setDni(hssfRow.getCell(c).getStringCellValue());
-                        	da.setNombre(hssfRow.getCell(c).getStringCellValue());
-                        	da.setApellido1(hssfRow.getCell(c).getStringCellValue());
-                        	da.setApellido2(hssfRow.getCell(c).getStringCellValue());
-                        	da.setNumExpediente((int) hssfRow.getCell(c).getNumericCellValue()); // ojo
-                        	da.setEmailInstitucional(hssfRow.getCell(c).getStringCellValue());
-                        	da.setEmailPersonal(hssfRow.getCell(c).getStringCellValue());
-                        	da.setTelefono((int) hssfRow.getCell(c).getNumericCellValue());
-                        	da.setMovil((int) hssfRow.getCell(c).getNumericCellValue());
-                        	da.setDireccionNotificacion(hssfRow.getCell(c).getStringCellValue());
-                        	da.setLocalidadNotificacion(hssfRow.getCell(c).getStringCellValue());
-                        	da.setProvinciaNotificacion(hssfRow.getCell(c).getStringCellValue());
-                        	da.setCpNotificacion((int) hssfRow.getCell(c).getNumericCellValue());
-                        	da.setFechaMatricula((Timestamp) hssfRow.getCell(c).getDateCellValue()); // ojo
-                        	da.setTurnoPreferente(hssfRow.getCell(c).getStringCellValue());
-                        	da.setGruposAsignados(hssfRow.getCell(c).getStringCellValue());
-                        	da.setNotaMedia((int) hssfRow.getCell(c).getNumericCellValue());
-                        	da.setCreditosSuperados((int) hssfRow.getCell(c).getNumericCellValue());
-                        	da.setCreditosFB((int) hssfRow.getCell(c).getNumericCellValue());
-                        	da.setCreditosOB((int) hssfRow.getCell(c).getNumericCellValue());
-                        	da.setCreditosCF((int) hssfRow.getCell(c).getNumericCellValue());
-                        	da.setCreditosPE((int) hssfRow.getCell(c).getNumericCellValue());
-                        	da.setCreditosTF((int) hssfRow.getCell(c).getNumericCellValue());              
-                    }
-                    listaDatos.add(da);
+                    
+                }else{    
+                	int c = 0; 
+                	
+                 	da.setDni(hssfRow.getCell(c).getStringCellValue());
+                	da.setNombre(hssfRow.getCell(c++).getStringCellValue());
+                	da.setApellido1(hssfRow.getCell(c++).getStringCellValue());
+                	da.setApellido2(hssfRow.getCell(c++).getStringCellValue());
+                	da.setNumExpediente((int) hssfRow.getCell(c++).getNumericCellValue());
+                	da.setEmailInstitucional(hssfRow.getCell(c+2).getStringCellValue());  // Columna NºARCHIVO saltada
+                	da.setEmailPersonal(hssfRow.getCell(c++).getStringCellValue());
+                	da.setTelefono((int) hssfRow.getCell(c++).getNumericCellValue());
+                	da.setMovil((int) hssfRow.getCell(c++).getNumericCellValue());
+                	da.setDireccionNotificacion(hssfRow.getCell(c++).getStringCellValue());
+                	da.setLocalidadNotificacion(hssfRow.getCell(c++).getStringCellValue());
+                	da.setProvinciaNotificacion(hssfRow.getCell(c++).getStringCellValue());
+                	da.setCpNotificacion((int) hssfRow.getCell(c++).getNumericCellValue());         
+                	da.setFechaMatricula(Timestamp.valueOf(hssfRow.getCell(c++).getStringCellValue())); //Formato de fecha y hora cambiada en documento previamente.
+                	da.setTurnoPreferente(hssfRow.getCell(c++).getStringCellValue());
+                	da.setGruposAsignados(hssfRow.getCell(c++).getStringCellValue());
+                	da.setNotaMedia((int) hssfRow.getCell(c++).getNumericCellValue());
+                	da.setCreditosSuperados((int) hssfRow.getCell(c++).getNumericCellValue());
+                	da.setCreditosFB((int) hssfRow.getCell(c++).getNumericCellValue());
+                	da.setCreditosOB((int) hssfRow.getCell(c++).getNumericCellValue());
+                	da.setCreditosCF((int) hssfRow.getCell(c++).getNumericCellValue());
+                	da.setCreditosPE((int) hssfRow.getCell(c++).getNumericCellValue());
+                	da.setCreditosTF((int) hssfRow.getCell(c++).getNumericCellValue());   
+                	
+                	listaDatos.add(da);
                 }
             }            
         } catch (FileNotFoundException fileNotFoundException) {
@@ -88,8 +82,6 @@ public class DatosEJB implements DatosEJBInterface {
                 System.out.println("Error al procesar el fichero después de cerrarlo: " + ex);
             }
         }
-	
-
 		return listaDatos;
 	}
 
